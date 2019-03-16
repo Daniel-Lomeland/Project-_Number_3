749172b457950b7e2ef5c679591b836f01336007
Scott Harvey <Scottharvey44@gmail.com>
2019-03-16 12:25:00 -0500

    updating time series chart

--- Time Series Bubble Plot/app.js
+++
@@ -1,8 +0,0 @@
-d3.csv("aac_shelter_outcomes_eng.csv", function(error, shelterData){
-
-  // Throw an error if one occurs
-  if (error) throw error;
-
-  // Print the sheltersData
-  console.log(shelterData);
-  });
