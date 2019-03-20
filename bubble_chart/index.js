4e45bb596cb6ac598e75038dddfffeeb43758f5e
muhlis cokadar <muhliscokadar@gmail.com>
2019-03-20 11:01:55 -0500

    git pull

--- bubble_chart/index.js
+++ bubble_chart/index.js
@@ -2,92 +2,68 @@ $(document).ready(function () {
     var bubbleChart = new d3.svg.BubbleChart({
       supportResponsive: true,
       //container: => use @default
-      size: 7000,
+      size: 1400,
       //viewBoxSize: => use @default
-      innerRadius: 600 / 3.5,
+      innerRadius: 700 / 2.5,
       //outerRadius: => use @default
-      radiusMin: 70,
+      radiusMin: 60,
       //radiusMax: use @default
       //intersectDelta: use @default
       //intersectInc: use @default
       //circleColor: use @default
       data: {
         items: [
-        //   {text: "Java", count: "236"},
-        //   {text: ".Net", count: "382"},
-        //   {text: "Php", count: "170"},
-        //   {text: "Ruby", count: "123"},
-        //   {text: "D", count: "12"},
-        //   {text: "Python", count: "170"},
-        //   {text: "C/C++", count: "382"},
-        //   {text: "Pascal", count: "10"},
-        //   {text: "Something", count: "170"},
         
-            // {
-            //   breed: "Domestic Longhair Mix",
-            //   animal: "Cat",
-            //   count: 1228
-            // },
-            // {
-            //   breed: "Domestic Medium Hair Mix",
-            //   animal: "Cat",
-            //   count: 2323
-            // },
-            // {
-            //   breed: "Domestic Shorthair",
-            //   animal: "Cat",
-            //   count: 386
-            // },
+            {
+              breed: "Domestic Longhair Mix",
+              animal: "Cat",
+              count: 1228
+            },
+            {
+              breed: "Domestic Medium Hair Mix",
+              animal: "Cat",
+              count: 2323
+            },
+            {
+              breed: "Domestic Shorthair",
+              animal: "Cat",
+              count: 386
+            },
             // {
             //   breed: "Domestic Shorthair Mix",
             //   animal: "Cat",
             //   count: 23335
             // },
-            // {
-            //   breed: "Siamese Mix",
-            //   animal: "Cat",
-            //   count: 998
-            // },
-            // {
-            //   breed: "American Bulldog Mix",
-            //   animal: "Dog",
-            //   count: 314
-            // },
-            // {
-            //   breed: "Australian Cattle Dog Mix",
-            //   animal: "Dog",
-            //   count: 1059
-            // },
-            // {
-            //   breed: "Australian Shepherd Mix",
-            //   animal: "Dog",
-            //   count: 454
-            // },
-            // {
-            //   breed: "Beagle Mix",
-            //   animal: "Dog",
-            //   count: 384
-            // },
-            // {
-            //   breed: "Border Collie Mix",
-            //   animal: "Dog",
-            //   count: 646
-            // },
-            // {
-            //   breed: "Boxer Mix",
-            //   animal: "Dog",
-            //   count: 674
-            // },
-            // {
-            //   breed: "Cairn Terrier Mix",
-            //   animal: "Dog",
-            //   count: 330
-            // },
-            // {
-            //   breed: "Catahoula Mix",
-            //   animal: "Dog",
-            //   count: 476
-            // },
+            {
+              breed: "Siamese Mix",
+              animal: "Cat",
+              count: 998
+            },
+            {
+              breed: "Beagle Mix",
+              animal: "Dog",
+              count: 384
+            },
+            {
+              breed: "Border Collie Mix",
+              animal: "Dog",
+              count: 646
+            },
+            {
+              breed: "Boxer Mix",
+              animal: "Dog",
+              count: 674
+            },
+            {
+              breed: "Cairn Terrier Mix",
+              animal: "Dog",
+              count: 330
+            },
+            {
+              breed: "Catahoula Mix",
+              animal: "Dog",
+              count: 476
+            },
             // {
             //   breed: "Chihuahua Longhair Mix",
             //   animal: "Dog",
@@ -123,51 +99,51 @@ $(document).ready(function () {
             //   animal: "Dog",
             //   count: 4608
             // },
-            // {
-            //   breed: "Miniature Poodle Mix",
-            //   animal: "Dog",
-            //   count: 648
-            // },
-            // {
-            //   breed: "Miniature Schnauzer Mix",
-            //   animal: "Dog",
-            //   count: 392
-            // },
+            {
+              breed: "Miniature Poodle Mix",
+              animal: "Dog",
+              count: 648
+            },
+            {
+              breed: "Miniature Schnauzer Mix",
+              animal: "Dog",
+              count: 392
+            },
             // {
             //   breed: "Pit Bull Mix",
             //   animal: "Dog",
             //   count: 6135
             // },
-            // {
-            //   breed: "Pointer Mix",
-            //   animal: "Dog",
-            //   count: 325
-            // },
-            // {
-            //   breed: "Rat Terrier Mix",
-            //   animal: "Dog",
-            //   count: 456
-            // },
-            // {
-            //   breed: "Rottweiler Mix",
-            //   animal: "Dog",
-            //   count: 321
-            // },
-            // {
-            //   breed: "Siberian Husky Mix",
-            //   animal: "Dog",
-            //   count: 418
-            // },
-            // {
-            //   breed: "Staffordshire Mix",
-            //   animal: "Dog",
-            //   count: 350
-            // },
-            // {
-            //   breed: "Yorkshire Terrier Mix",
-            //   animal: "Dog",
-            //   count: 437
-            // },
+            {
+              breed: "Pointer Mix",
+              animal: "Dog",
+              count: 325
+            },
+            {
+              breed: "Rat Terrier Mix",
+              animal: "Dog",
+              count: 456
+            },
+            {
+              breed: "Rottweiler Mix",
+              animal: "Dog",
+              count: 321
+            },
+            {
+              breed: "Siberian Husky Mix",
+              animal: "Dog",
+              count: 418
+            },
+            {
+              breed: "Staffordshire Mix",
+              animal: "Dog",
+              count: 350
+            },
+            {
+              breed: "Yorkshire Terrier Mix",
+              animal: "Dog",
+              count: 437
+            },
             // {
             //   breed: "Bat",
             //   animal: "Other",
@@ -178,33 +154,33 @@ $(document).ready(function () {
             //   animal: "Other",
             //   count: 1286
             // },
-            // {
-            //   breed: "Raccoon Mix",
-            //   animal: "Other",
-            //   count: 465
-            // },
+            {
+              breed: "Raccoon Mix",
+              animal: "Other",
+              count: 465
+            },
            
           
             // {
-            //   breed: "Domestic Longhair Mix",
-            //   animal: "Cat",
-            //   count: 1228
-            // },
-            // {
-            //   breed: "Domestic Medium Hair Mix",
-            //   animal: "Cat",
-            //   count: 2323
-            // },
-            // {
-            //   breed: "Domestic Shorthair Mix",
-            //   animal: "Cat",
-            //   count: 23335
-            // },
-            // {
-            //   breed: "Siamese Mix",
-            //   animal: "Cat",
-            //   count: 998
-            // },
+            // //   breed: "Domestic Longhair Mix",
+            // //   animal: "Cat",
+            // //   count: 1228
+            // // },
+            {
+              breed: "Domestic Medium Hair Mix",
+              animal: "Cat",
+              count: 2323
+            },
+            {
+              breed: "Domestic Shorthair Mix",
+              animal: "Cat",
+              count: 23335
+            },
+            {
+              breed: "Siamese Mix",
+              animal: "Cat",
+              count: 998
+            },
             {
               breed: "Australian Cattle Dog Mix",
               animal: "Dog",
@@ -293,13 +269,13 @@ $(document).ready(function () {
                 textField: "count",
                 classed: {count: true},
                 style: {
-                  "font-size": "20px",
+                  "font-size": "15px",
                   "font-family": "Source Sans Pro, sans-serif",
                   "text-anchor": "middle",
                   fill: "white"
                 },
                 attr: {
-                  dy: "-30px",
+                  dy: "-20px",
                   x: function (d) {return d.cx;},
                   y: function (d) {return d.cy;}
                 }
@@ -308,7 +284,7 @@ $(document).ready(function () {
                 textField: "breed",
                 classed: {breed: true},
                 style: {
-                  "font-size": "14px",
+                  "font-size": "10px",
                   "font-family": "Source Sans Pro, sans-serif",
                   "text-anchor": "middle",
                   fill: "white"
@@ -323,7 +299,7 @@ $(document).ready(function () {
                 textField: "animal",
                 classed: {animal: true},
                 style: {
-                  "font-size": "20px",
+                  "font-size": "15px",
                   "font-family": "Source Sans Pro, sans-serif",
                   "text-anchor": "middle",
                   fill: "white"
@@ -337,16 +313,16 @@ $(document).ready(function () {
             ],
             centralFormat: [
               {// Line #0
-                style: {"font-size": "40px"},
+                style: {"font-size": "50px"},
                 attr: {}
               },
               {// Line #1
-                style: {"font-size": "30px"},
-                attr: {dy: "10px"}
+                style: {"font-size": "40px"},
+                attr: {dy: "25px"}
               },
               {// Line #2
-                style: {"font-size": "30px"},
-                attr: {dy: "40px"}
+                style: {"font-size": "40px"},
+                attr: {dy: "70px"}
               }
 
             ]
